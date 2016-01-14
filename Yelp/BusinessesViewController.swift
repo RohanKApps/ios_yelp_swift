72c4477ca1777d81f8cf52a9008ecc48fb3c7aa3
Rohan K <rohankatakam@yahoo.com>
2016-01-13 19:01:31 -0600

    WorstSpots Original Commit
    -Working Yelp API
    -Working SearchBar function
    -(In Progress) Array element space editing loop in Search Function

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     var businesses: [Business]!
+    var names : [String] = ["asianfusion", "burgers"]
+    var input = String()
+    var categoryArray : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-//            self.businesses = businesses
-//            
-//            for business in businesses {
-//                println(business.name!)
-//                println(business.address!)
-//            }
-//        })
+        searchBar.showsScopeBar = true
+        searchBar.delegate = self
         
-        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+        Business.searchWithTerm("Restaurants", sort: .Distance, categories: names, deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
             
             for business in businesses {
@@ -33,20 +31,39 @@ class BusinessesViewController: UIViewController {
             }
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar){
+        input = searchBar.text!
+        print(input)
+        //Create Raw Array of Tags
+        categoryArray = input.characters.split{$0 == ","}.map(String.init)
+        
+        //Take Out first space in the Array
+        for(var i = 0; i < categoryArray.count; i++){
+            if categoryArray[i][0] == " "{
+                categoryArray[i] = String(categoryArray[i].characters.dropFirst())
+            }
+        }
+        
+        print(categoryArray)
     }
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//String Extension for getting char at nth location of String
+extension String {
+    
+    subscript (i: Int) -> Character {
+        return self[self.startIndex.advancedBy(i)]
     }
-    */
-
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = startIndex.advancedBy(r.startIndex)
+        let end = start.advancedBy(r.endIndex - r.startIndex)
+        return self[Range(start: start, end: end)]
+    }
 }
