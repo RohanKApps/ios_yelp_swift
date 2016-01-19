a780c012d47243b6805d6c6e7a045667d72e962f
Rohan K <rohankatakam@yahoo.com>
2016-01-18 23:06:19 -0600

    Finished App
    -Markers Placed on Locations
    -Camera Zooms Out

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -71,23 +71,94 @@ class BusinessesViewController: UIViewController, UISearchBarDelegate, UITableVi
         }
     }
     
-    // 2
+    
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        // 3
+        
         if status == .AuthorizedWhenInUse {
             
-            // 4
+            
             manager.startUpdatingLocation()
             
-            //5
+            
             mapView.myLocationEnabled = true
             mapView.settings.myLocationButton = true
         }
     }
     
-    // 6
+    func getFullAddress(var address: String) -> String{
+        var fullAddresses : [String] = []
+        var rawJSON : String = String()
+        var returnValue : String = ""
+        
+        //Create Request
+        let inst = address
+        var edit = inst.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        var finalIn = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=\(edit)&types=geocode&language=en&key=AIzaSyCWK3r4SS4N8_n3vPvVvUmwdRikY9tABSw"
+        
+        print(finalIn)
+        
+        func parseJSON(input: String) {
+            
+            // Setup the session to make REST GET call.  Notice the URL is https NOT http!!
+            let postEndpoint: String = input
+            let session = NSURLSession.sharedSession()
+            let url = NSURL(string: postEndpoint)!
+            
+            // Make the POST call and handle it in a completion handler
+            session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+                // Make sure we get an OK response
+                guard let realResponse = response as? NSHTTPURLResponse where
+                    realResponse.statusCode == 200 else {
+                        print("Not a 200 response")
+                        return
+                }
+                
+                // Read the JSON
+                do {
+                    if let ipString = NSString(data:data!, encoding: NSUTF8StringEncoding) {
+                        // Print what we got from the call
+                        rawJSON = ipString as String
+                        /*print(rawJSON)*/
+                        
+                        ////////////////////
+                        
+                        var data = rawJSON.dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: false)
+                        do {
+                            var json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+                            
+                            if let dict = json as? [String: AnyObject] {
+                                if let weather = dict["predictions"] as? [AnyObject] {
+                                    for dict2 in weather {
+                                        let description = dict2["description"] as! String
+                                        fullAddresses.append(description)
+                                    }
+                                    returnValue = fullAddresses[0]
+                                    
+                                }
+                            }
+                            
+                        }
+                        catch {
+                            print(error)
+                        }
+                    }
+                } catch {
+                    print("Error")
+                }
+                
+            }).resume()
+        }
+        
+        parseJSON(finalIn)
+        while(returnValue == ""){
+            if returnValue != ""{
+                return returnValue
+            }
+        }
+        return address
+    }
 
-    
+
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return names.count
     }
@@ -132,14 +203,42 @@ class BusinessesViewController: UIViewController, UISearchBarDelegate, UITableVi
         }    
     }
     
-    func plotAddresses(addresses: [String]){
-        let  position = CLLocationCoordinate2DMake(10, 10)
-        let marker = GMSMarker(position: position)
-        marker.title = "Hello World"
-        marker.map = mapView
-        for(var i = 0; i < addresses.count; i++){
-            print("Im done")
+    func plotAddresses(addressArr: [String]){
+        var markersArray : [GMSMarker] = []
+        var placeNames = names
+        var addressNames = addresses
+        var iterateNames = 0
+        var iterateAddresses = 0
+        print(placeNames)
+        for(var i = 0; i < addressArr.count; i++){
+            let address = addressArr[i]
+            let geocoder = CLGeocoder()
+            geocoder.geocodeAddressString(address, completionHandler: {(placemarks, error) -> Void in
+                if((error) != nil){
+                    print("Error", error)
+                }
+                if let placemark = placemarks?.first {
+                    let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                    let  position = coordinates
+                    let marker = GMSMarker(position: position)
+                    marker.title = placeNames[iterateNames]
+                    marker.snippet = addressNames[iterateAddresses]
+                    markersArray.append(marker)
+                    marker.map = self.mapView
+                    if iterateNames == placeNames.count - 1 {
+                        iterateNames = placeNames.count - 1
+                    } else {
+                        iterateNames++
+                    }
+                    if iterateAddresses == addressNames.count - 1 {
+                        iterateAddresses = addressNames.count - 1
+                    } else {
+                        iterateAddresses++
+                    }
+                }
+            })
         }
+        mapView.animateToZoom(6)
     }
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar){
@@ -201,11 +300,19 @@ class BusinessesViewController: UIViewController, UISearchBarDelegate, UITableVi
                 }
                 //Optomize Arrays to show the five worst
                 self.names = self.optomizeArray(self.names)
+                
+                for(var i = 0; i < self.addresses.count; i++){
+                    if self.getFullAddress(self.addresses[i]) != self.addresses[i]{
+                        self.addresses[i] = self.getFullAddress(self.addresses[i])
+                    }
+                }
+                
                 self.addresses = self.optomizeArray(self.addresses)
                 self.ratingsUrl = self.optomizeArray(self.ratingsUrl)
                 self.distances = self.optomizeArray(self.distances)
                 self.reviewCounts = self.optomizeArray(self.reviewCounts)
-
+                
+                
                 self.plotAddresses(self.addresses)
                 self.tableView.reloadData()
                 
